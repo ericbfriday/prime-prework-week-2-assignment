3ee3507be71e45e6d3752c404cb1452c820d54c9
Eric Friday <ericfriday@gmail.com>
2017-07-28 23:14:34 -0500

    Saving Partial Work - However code is not functioning properly

--- assignment/1_alerts.js
+++ assignment/1_alerts.js
@@ -1,6 +1,8 @@
 // Is the JavaScript file sourced? Check in the browser console by
 // right clicking and selecting 'Inspect'. If the script is properly
 // sourced you will see the log message: 'Script sourced!'.
+
+// EF note - The script does source successfully! Yay!
 console.log('Script sourced!');
 
 /**
@@ -11,26 +13,51 @@ console.log('Script sourced!');
  * - that they should try again.
  */
 
+
 // 1. Create a variable that takes in user input via the prompt function
 // that asks "Of what animal am I thinking?".
 
-// var animal =
+var animal = prompt("Of what animal am I thinking?");
 
 // 2. Write a conditional statement (if/else) that alerts
 // - "How did you know?!" if the user guesses "turtle" or
 // - "Try again." if the user did not guess "turtle" correctly.
 // IMPORTANT: Use these exact strings: 'Try again.' and 'How did you know?!'.
 
+if (animal === "turtle") {
+  alert("How did you know?!");
+}
+
+else {
+  alert("Try again.");
+}
+
 // 3. Refactor your code above into the "animalGuess" function below.
 // Refactor means the website should behave the same to the users,
 // but the code is written differently.
+
 // Think about the variable "animalName" and how it is different than "animal".
 // Call the function by passing the animal variable into it
 
 function animalGuess(animalName) {
+  animalName = prompt("Of what animal am I thinking?");
   // conditional statement goes in here
+
+  if (animalName === "turtle") {
+    alert("How did you know?!");
+  }
+
+  else {
+    alert("Try again.");
+  }
 }
 
+animalGuess(animal);
 // 4. Answer the question: What will "console.log(animalName)" log outside the
 // "animalGuess" function?
-// Your answer here in a comment
+
+//EF note - It will return an undefined error, as the parameter 'animalName' is not
+//defined outside of the function animalGuess. animalName is limited in 'scope'
+//to be local to animalGuess. If it were defined outside of the function, then
+//it could be defined 'globally', and it could be called by the console.log()
+//command.
--- assignment/2_types.js
+++ assignment/2_types.js
@@ -12,29 +12,66 @@
 
 var someNumber = 8;
 // make sure to test all the possible inputs.
-// var someString =
+var someString = "Salad";
 
-theDoubler(someNumber);
+var someBoolean = typeof true;
+
+var someObject = typeof {a: 1};
+
+var someOther = undefined;
+
+// EF Note - moved this call down below
+// theDoubler(someNumber);
 
 function theDoubler(someInput) {
 	// typeof is a built in javascript tool that allows us to find the type of an object
 	// for number, we have given an example of how that works
 	if (typeof someInput == "number") {
 		// if type is number, log that number multiplied by 2
+		console.log(someInput * 2);
 	}
 
+	else if (typeof someInput == "string") {
+		console.log(someInput + someInput);
+	}
 	// if type is string pass back that string twice
 	// e.g. sending "someString" in, should log "someStringsomeString"
 
-
+	else if (typeof someInput == boolean) {
+		if (someInput == "False") {
+			console.log("I won't double this");
+		}
+		else if (someInput == "True") {
+			console.log("Super-duper true");
+		}
+	}
 	// if the user inputs a boolean (true or false)
 	// log "I won't double this" if it is false
 	// log "Super-duper true" if it is true
-
-
+	else if (typeof someInput == object) {
+		console.log("everything is an object");
+	}
 	// if it's none of those, but it is an object, log "everything is an object"
 
-
+	else {
+		console.log("I can't double this")
+	}
 	// if it is anything else, print out "I can't double this"
 
 }
+/*
+console.log("Checking number");
+theDoubler(someNumber);
+
+console.log("checking strings");
+theDoubler(someString);
+
+console.log("checking booleans");
+theDoubler(someBoolean);
+
+console.log("checking objects");
+theDoubler(someObject);
+
+console.log("checking other");
+theDoubler(someOther);
+*/
